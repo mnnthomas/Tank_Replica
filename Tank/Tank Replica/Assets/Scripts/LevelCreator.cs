0da12d7411db6f256718521a1bceb32e31a9262d
DeathQuake <mnn.thomas@gmail.com>
2020-01-23 22:23:14 +0530

    Initial commit

---
+++ Tank/Tank Replica/Assets/Scripts/LevelCreator.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class ColorMap
+{
+    public string _TileName;
+    public GameObject _TileObject;
+    public Color32 _ReferenceColor;
+}
+
+public class LevelCreator : MonoBehaviour
+{
+    public float _TileOffset;
+    public float _TileSize;
+    public int _GridSize;
+    public GameObject _GridParent;
+    public List<ColorMap> _ColorMaps = new List<ColorMap>();
+    public List<Texture2D> _LevelTextures = new List<Texture2D>();
+
+    public static LevelCreator pInstance{get; private set;}
+        
+    private GameObject[,] mTiles;
+
+    void Awake()
+    {
+        if(pInstance == null)
+            pInstance = this;
+    }
+
+    private void Start()
+    {
+        LoadNextLevel();
+    }
+
+    public void LoadNextLevel()
+    {
+        InstantiateGrid(_LevelTextures[0]);
+    }
+
+    void InstantiateGrid(Texture2D levelTex)
+    {
+        Texture2D tempTex = new Texture2D(levelTex.width, levelTex.width, levelTex.format, levelTex.mipmapCount, false);
+        Graphics.CopyTexture(levelTex, tempTex);
+        
+        mTiles = new GameObject[_GridSize, _GridSize];
+        for (int i = 0; i < _GridSize; i++)
+        {
+            for (int j = 0; j < _GridSize; j++)
+            {
+                Color32 pixelColor = tempTex.GetPixel(j, i);
+                GameObject mCurTile = GetTileObject(pixelColor);
+
+                if (mCurTile != null)
+                {
+                    mTiles[i, j] = Instantiate(mCurTile);
+                    mTiles[i, j].gameObject.name = mCurTile.name;
+                    mTiles[i, j].transform.SetParent(_GridParent.transform);
+                    mTiles[i, j].transform.position = new Vector3(j * _TileSize + j * _TileOffset, 0f, i * _TileSize + i * _TileOffset);
+                    mTiles[i, j].transform.position -= new Vector3(_GridSize / 2, 0, _GridSize / 2);
+                }
+            }
+        }
+    }
+
+    public GameObject GetTileObject(Color32 color)
+    {
+        ColorMap tileMap = _ColorMaps.Find(x => (color.r == x._ReferenceColor.r && color.g == x._ReferenceColor.g && color.b == x._ReferenceColor.b));
+        if (tileMap != null)
+            return tileMap._TileObject;
+        return null;
+    }
+
+    private void OnDestroy()
+    {
+        pInstance = null;
+    }
+}
---
+++ Tank/Tank Replica/Assets/Scripts/MovementController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class MovementController : MonoBehaviour
+{
+    private CharacterController mCharController;
+    private string mHorizontal;
+    private string mVertical;
+    private float mSpeed;
+    private bool mAllowInput;
+    private bool mIsInitialized;
+
+    void Start()
+    {
+        mCharController = GetComponent<CharacterController>();
+        InitMovement(5f, "Horizontal", "Vertical"); //Hardcoding Init
+    }
+
+    /// <summary>
+    /// Inits movement update
+    /// </summary>
+    /// <param name="speed">movement speed</param>
+    /// <param name="horizontalAxis">name of horizontal axis</param>
+    /// <param name="verticalAxis">name of vertical axis</param>
+    public void InitMovement(float speed, string horizontalAxis, string verticalAxis)
+    {
+        mIsInitialized = true;
+        mHorizontal = horizontalAxis;
+        mVertical = verticalAxis;
+        mSpeed = speed;
+        Pause(false);
+    }
+
+    /// <summary>
+    /// Pauses movement controller's update
+    /// </summary>
+    /// <param name="value">mAllowInput</param>
+    public void Pause(bool value)
+    {
+        mAllowInput = !value;
+    }
+
+    void Update()
+    {
+        HandlePlayerInputs();
+    }
+
+    public void UpdateSpeed(float speed)
+    {
+        mSpeed = speed;
+    }
+
+    void HandlePlayerInputs()
+    {
+        if (mAllowInput && mIsInitialized)
+        {
+            Vector3 movementVector = new Vector3(Input.GetAxis(mHorizontal), 0, Input.GetAxis(mVertical)) * mSpeed * Time.deltaTime;
+            mCharController.Move(movementVector);
+        }
+    }
+}
+
